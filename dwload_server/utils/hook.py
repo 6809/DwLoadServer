ebbc8dfe4295d2cdd78603a65114c0b6d1c24813
JensDiemer <git@jensdiemer.de>
2014-11-17 17:39:47 +0100

    backup rename before create ASCII listing

--- dwload_server/utils/hook.py
+++ dwload_server/utils/hook.py
@@ -9,11 +9,13 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
+import time
 import logging
 import os
+
 from dragonlib.api import Dragon32API
-from dragonlib.core.binary_files import BinaryFile
 from dwload_server import constants
+
 
 log = logging.getLogger(__name__)
 
@@ -47,11 +49,12 @@ def register_post_hook(op_code):
     def inner(func):
         DW_HOOKS.register_post_hook(op_code, func)
         return func
+
     return inner
 
 
 # class SaveASCII(object):
-#     def __init__(self):
+# def __init__(self):
 #         self.filepath=None
 #
 #     def set_filepath(self, filepath):
@@ -69,6 +72,32 @@ def register_post_hook(op_code):
 #     filepath = os.path.join(server.root_dir, filename)
 #     save_ascii.set_filepath(filepath)
 
+def backup_rename(filepath):
+    """
+    renamed filepath if it's a existing file by expand filename with last modified time
+
+    :param filepath: source file that should be renamed
+    :return:
+    """
+    if os.path.isfile(filepath):
+        log.info("Create a backup of the old %r", filepath)
+
+        mtime = os.path.getmtime(filepath)
+        mtime = time.localtime(mtime)
+
+        bak_filepath = filepath + time.strftime("-%Y%m%d-%H%M%S")
+        if not os.path.isfile(bak_filepath + ".bak"):
+            bak_filepath += ".bak"
+        else:
+            count = 1
+            while os.path.isfile(bak_filepath + "-%01i.bak" % count):
+                count += 1
+            bak_filepath += "-%01i.bak" % count
+
+        os.rename(filepath, bak_filepath)
+        log.info("Backup as: %r", bak_filepath)
+
+
 @register_post_hook(constants.OP_WRITE)
 def save_ascii_post_write_hook(server, filepath, lsn):
     log.critical("Hook info: Filepath %r LSN: %i", filepath, lsn)
@@ -84,46 +113,31 @@ def save_ascii_post_write_hook(server, filepath, lsn):
 
     api = Dragon32API()
 
-    ascii_listing=api.bin2bas(content)
+    ascii_listing = api.bin2bas(content)
 
-    bas_filepath = filepath+".bas"
+    bas_filepath = filepath + ".bas"
+    backup_rename(bas_filepath)
 
     log.info("Create %r...", bas_filepath)
     with open(bas_filepath, "w") as f:
         f.write("\n".join(ascii_listing))
 
 
+if __name__ == '__main__':
+    from dragonlib.utils.logging_utils import setup_logging
 
-#
-# if __name__ == '__main__':
-#     from dragonlib.utils.logging_utils import setup_logging
-#
-#     setup_logging(
-#         level=1 # hardcore debug ;)
-# #         level=10  # DEBUG
-# #         level=20  # INFO
-# #         level=30  # WARNING
-# #         level=40 # ERROR
-# #         level=50 # CRITICAL/FATAL
-# #         level=99
-#     )
-#
-#     filepath=os.path.expanduser("~/dwload-files/AUTOLOAD.DWL")
-#
-#     import struct
-#     header = struct.pack("B", 0x55)
-#     print(type(header), repr(header))
-#     data = struct.unpack("B", header)
-#     print(repr(data))
-#
-#     with open(filepath, "rb") as f:
-#         content = f.read()
-#
-#     # content = content.rstrip(b"\x00") # strip 256 Byte padding
-#     # content += b"\x00\x00\x00" # readd the striped BASIC list end terminator bytes
-#     print(type(content), repr(content))
-#
-#
-#
-#     # binary_file = BinaryFile()
-#     # binary_file.load_from_bin(content)
+    setup_logging(
+        level=1 # hardcore debug ;)
+        #         level=10  # DEBUG
+        #         level=20  # INFO
+        #         level=30  # WARNING
+        #         level=40 # ERROR
+        #         level=50 # CRITICAL/FATAL
+        #         level=99
+    )
+
+    filepath = os.path.expanduser("~/dwload-files/AUTOLOAD.DWL")
+
+    backup_rename(filepath)
+    backup_rename(filepath)
+    backup_rename(filepath)
