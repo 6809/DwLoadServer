9ef24505369047ec4e41062a11b81dca5f15a0fc
JensDiemer <git@jensdiemer.de>
2022-09-10 18:23:00 +0200

    code cleanup

--- dwload_server/server_serial.py
+++ dwload_server/server_serial.py
@@ -8,26 +8,14 @@
 """
 
 
-import os
 import logging
 import socket
 import sys
 import time
 
-from dwload_server.server_base import DwLoadServer, BaseServer
+import serial
 
-
-try:
-    import serial
-except ImportError as err:
-    raise ImportError("%s - Please install PySerial ! - http://pyserial.sourceforge.net" % err)
-
-try:
-    import dragonlib
-except ImportError as err:
-    raise ImportError("dragonlib from https://github.com/jedie/DragonPy is needed: %s" % err)
-
-from dragonlib.utils.logging_utils import setup_logging
+from dwload_server.server_base import BaseServer, DwLoadServer
 
 
 LOG_DEZ = False
@@ -81,14 +69,14 @@ class BeckerServer(BaseServer):
 
         if LOG_DEZ:
             log.debug("\tdez: %s", " ".join(["%i" % b for b in data]))
-        log.debug("\thex: %s", " ".join(["$%02x" % b for b in data]))
+        log.debug("\thex: %s", " ".join([f"${b:02x}" for b in data]))
         return data
 
     def write(self, data):
         log.debug("WRITE %i:", len(data))
         if LOG_DEZ:
             log.debug("\tdez: %s", " ".join(["%i" % b for b in data]))
-        log.debug("\thex: %s", " ".join(["$%02x" % b for b in data]))
+        log.debug("\thex: %s", " ".join([f"${b:02x}" for b in data]))
         self.conn.sendall(data)
 
 
@@ -104,11 +92,11 @@ class SerialServer(BaseServer):
         try:
             self.conn.open()
         except serial.serialutil.SerialException as err:
-            sys.stderr.write("\nERROR: Can't open serial %r !\n" % port)
+            sys.stderr.write(f"\nERROR: Can't open serial {port!r} !\n")
             sys.stderr.write("\nRight Port? Port not in use? User rights ok?\n")
             sys.stderr.write(
                 "Look at http://archive.worldofdragon.org/index.php?title=Dragon_32/64_Drivewire_Adapter for help!\n")
-            sys.stderr.write("\n(Origin error is: %s)\n\n" % err)
+            sys.stderr.write(f"\n(Origin error is: {err})\n\n")
             sys.exit(-1)
 
         log.debug("Settings for serial %r:", self.conn.name)
@@ -129,38 +117,17 @@ class SerialServer(BaseServer):
         data = self.conn.read(size)
         if LOG_DEZ:
             log.debug("\tdez: %s", " ".join(["%i" % b for b in data]))
-        log.debug("\thex: %s", " ".join(["$%02x" % b for b in data]))
+        log.debug("\thex: %s", " ".join([f"${b:02x}" for b in data]))
         return data
 
     def write(self, data):
         log.debug("WRITE %i:", len(data))
         if LOG_DEZ:
             log.debug("\tdez: %s", " ".join(["%i" % b for b in data]))
-        log.debug("\thex: %s", " ".join(["$%02x" % b for b in data]))
+        log.debug("\thex: %s", " ".join([f"${b:02x}" for b in data]))
         self.conn.write(data)
 
 
 def run_serial_server(root_dir, port):
     dwload_server = SerialServer(port=port, root_dir=root_dir)
     dwload_server.serve_forever()
-
-
-if __name__ == '__main__':
-    print("\nDirect run, only for testing!", file=sys.stderr)
-    print("Please use cli!\n", file=sys.stderr, flush=True)
-
-    sys.argv += [
-        "--root_dir=~/dwload-files", "--log_level=10",
-        "serial"
-    ]
-    if sys.platform.startswith('win'):
-        sys.argv.append("--port=COM3")
-    else:
-        sys.argv.append("--port=/dev/ttyUSB0")
-
-
-    from dwload_server.dwload_server_cli import DwLoadServerCLI
-    cli = DwLoadServerCLI()
-    cli.run()
-
-    print(" --- END --- ")
